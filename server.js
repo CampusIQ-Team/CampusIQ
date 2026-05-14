080a55b0113f3af4843e5fb1a57b242471f5b2e2
Pandora-boxes <lingogreyling@gmail.com>
2026-05-14 10:14:20 +0200

    Testing deployment

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./Backend/config/db');
 
 connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./Backend/routes/auth');
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
